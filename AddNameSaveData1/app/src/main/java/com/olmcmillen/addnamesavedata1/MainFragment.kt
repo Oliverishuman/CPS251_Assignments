9019a5a660d7e7fd1341c8f257c18eb2d5ce3730
Oliverishuman <mcmilleno@yahoo.com>
2023-10-12 20:07:08 -0400

    Final Updates before submission 10-12-23 8:07:05 PM

--- AddNameSaveData1/app/src/main/java/com/olmcmillen/addnamesavedata1/MainFragment.kt
+++ AddNameSaveData1/app/src/main/java/com/olmcmillen/addnamesavedata1/MainFragment.kt
@@ -1,8 +1,6 @@
 package com.olmcmillen.addnamesavedata1
 
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,20 +8,12 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.olmcmillen.addnamesavedata1.databinding.FragmentMainBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [MainFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainFragment() : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
@@ -32,11 +22,6 @@ class MainFragment() : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
-    }
-
-    constructor(parcel: Parcel) : this() {
-        param1 = parcel.readString()
-        param2 = parcel.readString()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,9 +34,10 @@ class MainFragment() : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_main, container, false)
+
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -71,10 +57,9 @@ class MainFragment() : Fragment() {
             if (binding.enterName.text.isNotEmpty()){
                 viewModel.setNames(binding.enterName.text.toString())
                 binding.namesView.text = viewModel.getNames()
-//                this.binding.namesView.text = this.binding.namesView.text.toString() + "\n" + binding.enterName.text.toString()
             }
             else {
-                this.binding.namesView.text = "No Name Entered"
+                this.binding.namesView.text = getString(R.string.noNameEntered)
             }
         }
     }
