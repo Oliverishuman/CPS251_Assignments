95d5b9e928b6425345e62f0294d47c8990bae681
Oliverishuman <mcmilleno@yahoo.com>
2023-10-30 19:40:12 -0400

    Initial upload for NavigationDemo in class folder

---
+++ InClass/NavigationDemo/app/src/main/java/com/olmcmillen/navigationdemo/FirstFragment.kt
@@ -0,0 +1,48 @@
+package com.olmcmillen.navigationdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.olmcmillen.navigationdemo.databinding.FragmentFirstBinding
+
+
+
+class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            val action: FirstFragmentDirections.MainToSecond  =
+                FirstFragmentDirections.mainToSecond()
+            action.message = binding.userText.text.toString()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ InClass/NavigationDemo/app/src/main/java/com/olmcmillen/navigationdemo/SecondFragment.kt
@@ -0,0 +1,51 @@
+package com.olmcmillen.navigationdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.net.Uri
+import com.olmcmillen.navigationdemo.databinding.FragmentSecondBinding
+
+
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_second, container, false)
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        arguments?.let {
+            val args = SecondFragmentArgs.fromBundle(it)
+            binding.argText.text = args.message
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
