77bd7e9c31c3e105339cc2e9a4123cc96f008de5
Oliverishuman <mcmilleno@yahoo.com>
2023-11-06 19:20:55 -0500

    Moved arrays to separate class and got app working from there

--- RecycleViewProject/app/src/main/java/com/olmcmillen/recycleviewproject/RecyclerAdapter.kt
+++ RecycleViewProject/app/src/main/java/com/olmcmillen/recycleviewproject/RecyclerAdapter.kt
@@ -6,8 +6,12 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.olmcmillen.recycleviewproject.Data
 
 class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
+
+    val thisData = Data();
+
     class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
         var itemImage: ImageView
         var itemTitle: TextView
@@ -27,27 +31,13 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
     }
 
     override fun getItemCount(): Int {
-        return titles.size
+        return thisData.titles.size
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
-        viewHolder.itemTitle.text = titles[i]
-        viewHolder.itemDetail.text = details[i]
-        viewHolder.itemImage.setImageResource(images[i])
+        viewHolder.itemTitle.text = thisData.titles[i]
+        viewHolder.itemDetail.text = thisData.details[i]
+        viewHolder.itemImage.setImageResource(thisData.images[i])
     }
-
-
-    private val titles = arrayOf("Chapter One",
-        "Chapter Two", "Chapter Three", "Chapter Four",
-        "Chapter Five", "Chapter Six", "Chapter Seven",
-        "Chapter Eight")
-    private val details = arrayOf("Item one details", "Item two details",
-        "Item three details", "Item four details",
-        "Item five details", "Item six details",
-        "Item seven details", "Item eight details")
-    private val images = intArrayOf(R.drawable.android_image_1,
-        R.drawable.android_image_2, R.drawable.android_image_3,
-        R.drawable.android_image_4, R.drawable.android_image_5,
-        R.drawable.android_image_6, R.drawable.android_image_7,
-        R.drawable.android_image_7)
+    
 }
