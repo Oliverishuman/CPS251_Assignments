ad83bf126a5e55d90df123e311f5839ff04bc7e5
Oliverishuman <mcmilleno@yahoo.com>
2023-11-13 18:51:38 -0500

    Initial upload for ExplicitIntent project

---
+++ InClass/ExplicitIntent/app/src/main/java/com/olmcmillen/explicitintent/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.olmcmillen.explicitintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.olmcmillen.explicitintent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
