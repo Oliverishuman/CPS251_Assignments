f0e04a23fb0d1bc2e0c7b67242ec1679c60bbc37
Oliverishuman <mcmilleno@yahoo.com>
2023-11-13 21:34:30 -0500

    Initial upload for RecycleViewIntent project

---
+++ RecycleViewIntentsProject/app/src/main/java/com/olmcmillen/recycleviewintentsproject/RecyclerAdapter.kt
@@ -0,0 +1,43 @@
+package com.olmcmillen.recycleviewintentsproject
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
+
+    private val dataClass = Data()
+
+    class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.itemImage)
+            itemTitle = itemView.findViewById(R.id.itemTitle)
+            itemDetail = itemView.findViewById(R.id.itemDetail)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.card_layout, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return dataClass.titles.size
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+
+        viewHolder.itemTitle.text = dataClass.titles[(0..7).random()]
+        viewHolder.itemDetail.text = dataClass.details[(0..7).random()]
+        viewHolder.itemImage.setImageResource(dataClass.images[(0..7).random()])
+    }
+
+}
